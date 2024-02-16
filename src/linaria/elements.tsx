c0c19ca0c68003d13a474c8c936936ff5ea5a882
Roni <64074505+RonishRohan@users.noreply.github.com>
2024-02-16 12:05:40 +0530

    fixed some linting errors

--- src/linaria/elements.tsx
+++ src/linaria/elements.tsx
@@ -1,5 +1,4 @@
 import {styled} from "@linaria/react"
-import {motion} from "framer-motion"
 
 export const HeaderContainer = styled.div`
     width: 100%;
--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,6 @@ import "normalize.css"
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    
-    <App />
+    <App></App>
   </React.StrictMode>,
 )
